9ee6b8c3e0d3635d687c1c630fe5ab1f0dd202d3
Romain Haye <hayromain.rh@gmail.com>
2018-08-15 15:07:04 +0200

    Add global loading indicator service

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,41 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from "../shared/model/course";
-import {Lesson} from "../shared/model/lesson";
-import {CoursesService} from "../services/courses.service";
-import {UserService} from "../services/user.service";
-import {Observable} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../shared/model/course';
+import { Lesson } from '../shared/model/lesson';
+import { Observable } from 'rxjs';
 
 
 @Component({
-    selector: 'course-detail',
-    templateUrl: './course-detail.component.html',
-    styleUrls: ['./course-detail.component.css']
+  selector: 'course-detail',
+  templateUrl: './course-detail.component.html',
+  styleUrls: ['./course-detail.component.css']
 })
 export class CourseDetailComponent implements OnInit {
 
-    course$: Observable<Course>;
-    lessons$: Observable<Lesson[]>;
+  course$: Observable<Course>;
+  lessons$: Observable<Lesson[]>;
 
-    constructor(private route: ActivatedRoute,
-                private coursesService: CoursesService,
-                private userService: UserService) {
+  constructor(private route: ActivatedRoute) {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        this.course$ = this.route.params
-            .switchMap(params => this.coursesService.findCourseByUrl(params['id']))
-            .first()
-            .publishLast().refCount();
+    this.course$ = this.route.data.map(data => data['detail'][0]);
 
-        this.lessons$ = this.course$
-            .switchMap(course => this.coursesService.findLessonsForCourse(course.id))
-            .first()
-            .publishLast().refCount();
+    this.lessons$ = this.route.data.map(data => data['detail'][1]);
 
-    }
-
+  }
 
 }
